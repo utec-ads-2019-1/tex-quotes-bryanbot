22d623d57efc126f5ad182121b4d87c9e90ab308
Bryan D. Tumba <bryan.delama@utec.edu.pe>
2019-04-02 11:15:01 -0500

    Primer commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main()
+{
+	int count;
+	string parrafo;
+	getline(cin, parrafo);
+	char c = '"';
+	string reemplazo = "``";
+	string reemplazo2 = "''";
+	count = 0;
+	for (int i = 0; i < (int)parrafo.length(); ++i) {
+		if (parrafo[i] == c) {
+			    count++;
+				if(count % 2 == 0){
+				parrafo.replace(i, 1, reemplazo2);
+				}
+				else {
+				parrafo.replace(i, 1, reemplazo);
+				}
+		}
+	}
+	cout << parrafo << endl;
+	system("pause");
 }
